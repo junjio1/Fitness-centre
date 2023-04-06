b389d74118ede4f22c912f8fc10ee803aa9fdd8e
Junji Oshiro <junjio1@hotmail.com>
2023-04-06 11:12:22 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'module5',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
